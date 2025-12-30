5c63ee24406754c3ebf3f611a442e31db5c580f1
pattyrocks <pattynascimentos@hotmail.com>
2025-12-30 18:24:05 -0300

    organizing in folders

--- connection.py
+++ connection.py
@@ -1,40 +1,19 @@
-import tmdbsimple as tmdb # type: ignore
+import tmdbsimple as tmdb
 import os
+import duckdb
+import time
+import requests
+from datetime import datetime, timedelta
+import calendar
 
+# Set your TMDB API key from environment variables
+# Make sure you have TMDBAPIKEY set in your environment
 tmdb.API_KEY = os.getenv('TMDBAPIKEY')
 
-def get_movies_per_page():
+# Connect to the DuckDB database
+# This will create 'tmdb.duckdb' if it doesn't exist
+con = duckdb.connect(database='TMDB', read_only=False)
 
-    result = tmdb.Discover().movie(page=1)
-    movies_list_of_lists = []
+con.disconnect()
 
-    for i in range(1,3):
-
-        result_i = tmdb.Discover().movie(page=i)['results']
-        movies_list_of_lists.append(result_i)
-
-        i=+1
-
-    return movies_list_of_lists
-
-def get_info_per_page():
-
-    info_list_of_lists = []
-
-    for i in get_movies_per_page():
-
-        for dict in i:
-            movie_id = dict['id']
-            movie_info = tmdb.Movies(movie_id).info()
-
-            info_list_of_lists.append(movie_info)
-    i=+1
-
-    print(info_list_of_lists)
-
-get_info_per_page()
-    
-
-
-    
-
+con.close()
