6fefc1c162ade99294abfec828f5226531a86537
pattyrocks <pattynascimentos@hotmail.com>
2026-01-08 23:21:54 -0300

    change rule again

--- app/pages/Top_10_Movies.py
+++ app/pages/Top_10_Movies.py
@@ -27,7 +27,7 @@ try:
             popularity,
             vote_count,
             vote_average,
-            ((vote_count*0.2) * vote_average) AS score
+            vote_count * POWER(vote_average, 2) AS score
         FROM movies
         WHERE release_date >= '2025-01-01' AND release_date < '2026-01-01'
         ORDER BY score DESC, popularity DESC
@@ -55,6 +55,7 @@ try:
             use_container_width=True
         )
         st.caption("Rank based on score: number of votes * average rating 🏆👏🏽")
+            st.caption("Rank based on score: number of votes × (average rating²)")
 
 except Exception as e:
     st.error(f"Could not connect to MotherDuck: {e}")
