c290bb6c5c2cd7fe2296f4c666f8b87f5d861d54
pattyrocks <pattynascimentos@hotmail.com>
2026-01-08 23:30:41 -0300

    whateve I dont mind

--- app/pages/Top_10_Movies.py
+++ app/pages/Top_10_Movies.py
@@ -27,7 +27,7 @@ try:
             popularity,
             vote_count,
             vote_average,
-            (vote_count*0.2) * gamma(vote_average + 1) AS score
+            (vote_count*0.05) * gamma(vote_average + 1) AS score
         FROM movies
         WHERE release_date >= '2025-01-01' AND release_date < '2026-01-01'
         ORDER BY score DESC, popularity DESC
